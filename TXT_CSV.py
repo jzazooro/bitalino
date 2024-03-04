1ead79027304fceb2212b109af2662c2d2c5afa9
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-03-04 16:42:26 +0100

    Refactor TXT_CSV.py to improve readability and maintainability

--- TXT_CSV.py
+++ TXT_CSV.py
@@ -1,27 +1,31 @@
 import pandas as pd
 
-RUTA_TXT = "docs/data/flexiones_miguel.txt"
-RUTA_CSV = "docs/data/csv/flexiones_miguel.csv"
 
-with open(RUTA_TXT) as f:
-    lines = f.readlines()
+def txt_csv(RUTA_TXT, RUTA_CSV):
+    with open(RUTA_TXT) as f:
+        lines = f.readlines()
     canal1 = []
     canal2 = []
     canal3 = []
     canal4 = []
     canal5 = []
     intervals = []
-for i in range(3,len(lines)):
-    intervals.append(lines[i].split("\t")[0])
-    canal1.append(lines[i].split("\t")[1])
-    canal2.append(lines[i].split("\t")[2])
-    canal3.append(lines[i].split("\t")[3])
-    canal4.append(lines[i].split("\t")[4])
-    canal5.append(lines[i].split("\t")[5])
+    for i in range(3,len(lines)):
+        intervals.append(lines[i].split("\t")[0])
+        canal1.append(lines[i].split("\t")[1])
+        canal2.append(lines[i].split("\t")[2])
+        canal3.append(lines[i].split("\t")[3])
+        canal4.append(lines[i].split("\t")[4])
+        canal5.append(lines[i].split("\t")[5])
 
 
-columns = ["intervalos", "canal1", "canal2", "canal3", "canal4", "canal5"]
-data = [intervals, canal1, canal2, canal3, canal4, canal5]
+    columns = ["intervalos", "canal1", "canal2", "canal3", "canal4", "canal5"]
+    data = [intervals, canal1, canal2, canal3, canal4, canal5]
 
-df = pd.DataFrame(data, columns).T
-df.to_csv(RUTA_CSV, index=False)
+    df = pd.DataFrame(data, columns).T
+    df.to_csv(RUTA_CSV, index=False)
+    
+if __name__=="__main__":
+    RUTA_TXT = "docs/data/flexiones_miguel.txt"
+    RUTA_CSV = "docs/data/csv/flexiones_miguel.csv"
+    txt_csv(RUTA_TXT, RUTA_CSV)
